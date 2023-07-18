a632b4a388a72ff09ff7a0e8cbcc57f0aa4c5c23
Saad ur Rehman <125679304+saadrehman10@users.noreply.github.com>
2023-07-18 23:28:45 +0500

    saad

--- components/button/Button.js
+++ components/button/Button.js
@@ -1,17 +1,17 @@
 import styles from './buttonscss.module.css'
 import Bulb from '../bulbs/Bulb';
 import { useState } from 'react';
+
 export default function Button({ButtonText}) {
     let [whichBulbhold, setwhichBulb] = useState(false);
     const changeBulb = () => {
         setwhichBulb(true);
         global= document.querySelector('html')
         global.style.colorScheme = "light";
+       
     }
 
     return (
-        
-        
         <>
         <button  className={styles.button}  onClick={changeBulb}>
             <span className={styles.actualtext}>&nbsp;{ButtonText}&nbsp;</span>
--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,18 @@
-import Bulb from '@/components/bulbs/Bulb';
+
 import Button from '../components/button/Button';
 import styles from '../styles/Home.module.css';
-import { useState } from 'react';
-
 
 
 export default function Home() {
+  
+  
   return (
     
     <>
       <div className={styles.containerBulb}>
        <Button ButtonText="Litup" />
       </div>
+      
     </>
   )
 }
